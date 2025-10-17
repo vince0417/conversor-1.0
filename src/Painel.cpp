be342c164027193316bd38625942bec480c35aea
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2025-10-17 16:46:43 -0300

    Dashboard update

--- src/Painel.cpp
+++ src/Painel.cpp
@@ -17,18 +17,18 @@ Painel::~Painel()
 void Painel::painel(std::string& usuario, int& opcao, int& base){
     system("clear");
     std::cout << "----------------------Painel----------------------\n";
-    std::cout << " 1 - Decimal para Hexadecimal;\n";
-    std::cout << " 2 - Decimal para Octal;\n";
-    std::cout << " 3 - Decimal para Binario;\n";
-    std::cout << " 4 - Hexadecimal para Decimal;\n";
-    std::cout << " 5 - Hexadecimal para Octal;\n";
-    std::cout << " 6 - Hexadecimal para Binario;\n";
-    std::cout << " 7 - Octal para Decimal;\n";
-    std::cout << " 8 - Octal para Hexadecimal;\n";
-    std::cout << " 9 - Octal para Binario;\n";
-    std::cout << "10 - Binario para Decimal;\n";
-    std::cout << "11 - Binario para Hexadecimal;\n";
-    std::cout << "12 - Binario para octal.\n";
+    std::cout << "|  1 - Decimal para Hexadecimal;                 |\n";
+    std::cout << "|  2 - Decimal para Octal;                       |\n";
+    std::cout << "|  3 - Decimal para Binario;                     |\n";
+    std::cout << "|  4 - Hexadecimal para Decimal;                 |\n";
+    std::cout << "|  5 - Hexadecimal para Octal;                   |\n";
+    std::cout << "|  6 - Hexadecimal para Binario;                 |\n";
+    std::cout << "|  7 - Octal para Decimal;                       |\n";
+    std::cout << "|  8 - Octal para Hexadecimal;                   |\n";
+    std::cout << "|  9 - Octal para Binario;                       |\n";
+    std::cout << "| 10 - Binario para Decimal;                     |\n";
+    std::cout << "| 11 - Binario para Hexadecimal;                 |\n";
+    std::cout << "| 12 - Binario para octal.                       |\n";
     std::cout << "--------------------------------------------------\n";
 
     std::string escolha{usuario + ", escolha um número de acordo com a conversão desejada: "};
